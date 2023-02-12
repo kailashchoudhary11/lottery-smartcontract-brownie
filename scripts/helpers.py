65172b181a7630695381ca46a21ea4fd6de685d2
kai <kailashjat604@gmail.com>
2023-02-12 12:20:12 +0530

    Refactor integration test

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,6 +1,13 @@
-from brownie import network, VRFCoordinatorV2Mock, accounts, MockV3Aggregator, config, Contract
+from brownie import (
+    network,
+    VRFCoordinatorV2Mock,
+    accounts,
+    MockV3Aggregator,
+    config,
+    Contract,
+)
 
-# Aggregator 
+# Aggregator
 DECIMALS = 8
 INITIAL_VALUE = 200000000000
 
@@ -18,17 +25,19 @@ contract_to_mock = {
     "vrf_coordinator": VRFCoordinatorV2Mock,
 }
 
+
 def get_account(index=None, id=None):
     if index is not None:
         return accounts[index]
 
     if id is not None:
         return accounts.get(id)
-    
+
     if network.show_active() in LOCAL_BLOCKCHAIN_NETWORK:
         return accounts[0]
 
     return accounts.add(config["wallet"]["from_key"])
+
 
 def get_contract_address(contract_name):
     contract = contract_to_mock.get(contract_name)
@@ -38,6 +47,7 @@ def get_contract_address(contract_name):
         if len(contract) <= 0:
             deploy_mocks()
         return contract[-1].address
+
 
 def deploy_mocks():
     print("Current Active newtork is", network.show_active())
@@ -51,15 +61,17 @@ def deploy_mocks():
     VRFCoordinatorV2Mock.deploy(BASE_FEE, GAS_PRICE_LINK, {"from": account})
     print("Coordinator Mock Deployed!")
 
+
 def get_key_hash():
     return config["networks"][network.show_active()]["keyhash"]
+
 
 def get_sub_id():
     global SUB_ID
 
     if SUB_ID is not None:
         return SUB_ID
-    
+
     if network.show_active() not in LOCAL_BLOCKCHAIN_NETWORK:
         return config["networks"][network.show_active()]["sub_id"]
 
@@ -69,12 +81,13 @@ def get_sub_id():
     coordinator = VRFCoordinatorV2Mock[-1]
 
     print("Creating Subscription...")
-    sub_tx = coordinator.createSubscription({'from': get_account()})
+    sub_tx = coordinator.createSubscription({"from": get_account()})
     sub_tx.wait(1)
     print("Subscription Created!")
     sub_id = sub_tx.events["SubscriptionCreated"]["subId"]
     SUB_ID = sub_id
     return sub_id
+
 
 def add_consumer(consumer):
     coordinator = get_coordinator()
@@ -85,13 +98,16 @@ def add_consumer(consumer):
     add_tx.wait(1)
     print("Consumer Added!")
 
+
 def fund_subscription(sub_id, fund_amount=FUND_AMOUNT):
     coordinator = get_coordinator()
     coordinator.fundSubscription(sub_id, fund_amount, {"from": get_account()})
 
+
 def fulfill_request(request_id, consumer):
     coordinator = get_coordinator()
     coordinator.fulfillRandomWords(request_id, consumer, {"from": get_account()})
+
 
 def remove_consumer(consumer):
     print("Removing consumer...")
@@ -100,14 +116,20 @@ def remove_consumer(consumer):
     add_tx.wait(1)
     print("Consumer removed!")
 
+
 def get_sub_ids(address):
     coordinator = get_coordinator()
     return coordinator.getSubscriptionIds(address)
-    
+
+
 def get_coordinator():
     if network.show_active() in LOCAL_BLOCKCHAIN_NETWORK:
         if len(VRFCoordinatorV2Mock) <= 0:
             deploy_mocks()
 
         return VRFCoordinatorV2Mock[-1]
-    return Contract.from_abi("VRFCoordinatorV2", get_contract_address("vrf_coordinator"), VRFCoordinatorV2Mock.abi)
+    return Contract.from_abi(
+        "VRFCoordinatorV2",
+        get_contract_address("vrf_coordinator"),
+        VRFCoordinatorV2Mock.abi,
+    )
--- tests/test_lottery_integration.py
+++ tests/test_lottery_integration.py
@@ -14,12 +14,8 @@ def test_can_pick_winner():
     lottery.startLottery({"from": account})
     lottery.enterToLottery({"from": account, "value": lottery.getEntryFee()})
     lottery.enterToLottery({"from": account, "value": lottery.getEntryFee()})
-    account_balance = account.balance()
-    lottery_balance = lottery.balance()
     lottery.endLottery({"from": account})
     time.sleep(60)
     remove_consumer(lottery.address)
     assert lottery.winner() == account.address
     assert lottery.balance() == 0
-    # assert account.balance() == lottery_balance + account_balance
-
